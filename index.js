bf5416815401fb26d2bc9f8e21d7544b4f06e847
pbsmith82 <pbsmith82@gmail.com>
2021-03-17 20:26:40 -0400

    commit tree 4cbcfd1bacf682eeb0579a0d07e21f86d5dcdafe

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "david"
+
+
+function upperCaseCustomerName() {
+    customerName = "BOB"
+    return customerName
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "liam"
+    return leastFavoriteCustomer
+}
